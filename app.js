0a863ec8449eb3e7014ca587b2832d4413fc4b08
Minwoo <made_power@naver.com>
2023-08-05 19:22:35 +0900

    로그 기록 구현
    전투 기록이 표시되도록 구현하였다.

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound:0,
-            winner : null
+            winner : null,
+            logMessages:[]
         };
     },
     computed:{
@@ -55,11 +56,13 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.winner = null;
             this.currentRound = 0;
+            this.logMessage=[];
         },
         attackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(5,12);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player','attack',attackValue);
             this.attackPlayer(); 
             if(this.playerHealth < 0 ){
 
@@ -69,11 +72,13 @@ const app = Vue.createApp({
             
             const attackValue = getRandomValue(8,15);
             this.playerHealth -= attackValue;
+            this.addLogMessage('monster','attack',attackValue);
         },
         specialAttackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(10,25);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('monster','attack',attackValue);
             this.attackPlayer();
         },
         healPlayer(){
@@ -84,10 +89,17 @@ const app = Vue.createApp({
             } else{
                 this.playerHealth += healValue;    
             }
-            this.attackPlayer();       
+            this.addLogMessage('player','heal',healValue);      
         },
         surrender(){
             this.winner  ='monster';
+        },
+        addLogMessage(who, what, value ){
+            this.logMessages.unshift({
+                actionBy:who,
+                actionType:what,
+                actionValue: value
+            });
         }
     }
 });
